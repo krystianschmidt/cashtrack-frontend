3b7d09e1284addef08c9a2122b3cbb7e6c8bacca
Krystian Schmidt <krystian.schmidt@siemens.com>
2023-05-29 04:56:56 +0200

    feat: Add register/login functionality to frontend and backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from "./guards/auth.guard";
+import {LoginGuard} from "./guards/login.guard";
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./authentication/authentication.module').then( m => m.AuthenticationModule),
+    canActivate: [LoginGuard]
   }
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,11 +6,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {XhrInterceptor} from "./providers/xhr-interceptor";
+import {initApp} from "./providers/initApp";
+import {AuthenticationService} from "./services/authentification/authentication.service";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [
+    {provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },{
+      provide: HTTP_INTERCEPTORS,
+      useClass: XhrInterceptor,
+      multi: true
+    },{
+      provide: APP_INITIALIZER,
+      useFactory: initApp,
+      multi: true,
+      deps: [AuthenticationService]
+    }
+  ],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
