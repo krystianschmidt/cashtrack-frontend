739086f2f8442b6e7f1bd9db92d3bb28548e6feb
Krystian Schmidt <krystian.schmidt@siemens.com>
2023-05-29 04:58:26 +0200

    feat: Add new transaction functionality to backend and fix in frontend
    - Define/Implemented new transaction addition functionality
    - adjust frontend

--- src/app/tabs/tab5/components/profile-list/profile-list.component.ts
+++ src/app/tabs/tab5/components/profile-list/profile-list.component.ts
@@ -5,6 +5,7 @@ import {AddTransactionPageComponent} from "../../../add-transaction-page/add-tra
 import {SettingsModalComponent} from "./settings-modal/settings-modal.component";
 import {CategorysModalComponent} from "./categorys-modal/categorys-modal.component";
 import {ExportDataModalComponent} from "./export-data-modal/export-data-modal.component";
+import {AuthenticationService} from "../../../../services/authentification/authentication.service";
 
 @Component({
   selector: 'profile-list',
@@ -16,7 +17,8 @@ export class ProfileListComponent  implements OnInit {
     private navCtrl: NavController,
     private modalCtrl: ModalController,
     private modalController: ModalController,
-    private routerOutlet: IonRouterOutlet
+    private routerOutlet: IonRouterOutlet,
+    private authService: AuthenticationService,
   ) {}
 
   ngOnInit() {}
@@ -55,5 +57,8 @@ export class ProfileListComponent  implements OnInit {
   logout() {
     // Perform logout logic here
     this.modalCtrl.dismiss();
+    this.authService.logout().then(() => {
+      this.navCtrl.navigateRoot('/auth/login');
+    });
   }
 }
